c2fb3abcda5a5d281161aa9956cfd69f84fafe89
hecker40 <166958948+clementhart999@users.noreply.github.com>
2024-04-28 14:45:46 -0700

    Add pymongo database

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
 
+client = MongoClient("localhost", 27017)
+
+db = client.Users
+
+print(db)
+
 app = Flask('app')
 
 @app.route('/')
